54052a966cf2dd242aaa6025a63954d31c313ea8
darkskoll <santiago.cifuentesg@udea.edu.co>
2026-03-04 21:36:58 -0500

    tarea

---
+++ Tarea.py
@@ -0,0 +1,24 @@
+class Pacientes():
+    def __init__(self):
+        self.__Nombre = ""
+        self.__Cedula = 0
+        self.__Genero = ""
+        self.__Servicio = ""
+    
+    def verNombres(self):
+        return self.__Nombre
+    def verCedula(self):
+        return self.__Cedula
+    def verGenero(self):
+        return self.__Genero
+    def verServicio(self):
+        return self.__Servicio
+    
+    def asignarNombre(self,n):
+        self.__Nombre = n
+    def asignarCedula(self,c):
+        self.__Cedula = c
+    def asignarGenero(self,g):
+        self.__Genero = g
+    def asignarServicio(self,s):
+        self.__Servicio = s
