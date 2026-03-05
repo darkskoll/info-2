2cf2d16f545490d5ebe9978689cfdd3de2caaaef
darkskoll <santiago.cifuentesg@udea.edu.co>
2026-03-04 22:34:36 -0500

    Tarea_Version_2

--- Tarea.py
+++ Tarea.py
@@ -22,3 +22,50 @@ class Pacientes():
         self.__Genero = g
     def asignarServicio(self,s):
         self.__Servicio = s
+
+class Sistema:
+    def __init__(self):
+        self.__lista_pacienta = []
+        
+        self.__numero_pascientes = len(self.__lista_pacientes)
+        
+    def ingresarPacientes(self):
+        Nombre = input("ingrese Nombre delñ paciente: ")
+        Cedula = int(input("Ingrese la cedulña del paciente: "))
+        Genero = input("ingrese el genero del paciente: ")
+        Servicio = input("Ingrese el servicio: ")
+        
+        P = Pacientes()
+        P.asignarNombre(Nombre)
+        P.asignarCedula(Cedula)
+        P.asignarGenero(Genero)
+        P.asignarServicio(Servicio)
+        
+        self.__lista_pacienta.append(P)
+        
+        self.__numero_pascientes = len(self.__lista_pacientes)
+        
+    def VerNumeroPacientes(self):
+        return self.__numero_pascientes
+    def VerDatosPacientes(self):
+        for i in self.__lista_pacienta:
+            if  Cedula == Pacientes.verCedula():
+                print("Nombre: " + Pacientes.verNombres())
+                print("Cedula: " + str(Pacientes.verCedula()))
+                print("Genero: " + Pacientes.verGenero())
+                print("Servicio: " + Pacientes.verServicio())
+                
+mi_sistema = Sistema()
+
+while True:
+    opcion = int(input("1. Ingresar paciente - 2. Ver numero de pacientes - 3. Ver datos de paciente - 4. Salir"))
+    if opcion == 1:
+        mi_sistema.ingresarPacientes()
+    elif opcion == 2:
+        print("Ahora hay " + str(mi_sistema.VerNumeroPacientes()))
+    elif opcion == 3:
+        mi_sistema.VerDatosPacientes()
+    elif opcion ==4:
+        break
+    else:
+        print("Opcion no valida")
